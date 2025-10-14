cfddc0baaac2e4faf3789c00e669b9191985640f
Alex Yanovsky <alexyano159@gmail.com>
2025-10-14 14:48:23 +0300

    Refactor Memory_Address_Register_tb for address width

--- CPU_Project/Memory_Address_Register_tb/Memory_Address_Register_tb.v
+++ CPU_Project/Memory_Address_Register_tb/Memory_Address_Register_tb.v
@@ -1,27 +1,31 @@
 module Memory_Address_Register_tb;
-parameter DATA_WIDTH = 32;
+parameter ADDR_WIDTH = 8;
 reg clk;
 reg reset;
-reg [DATA_WIDTH-1:0] address_in;
+reg [ADDR_WIDTH-1:0] address_in;
 reg load;
-wire [DATA_WIDTH-1:0] address_out;
+wire [ADDR_WIDTH-1:0] address_out;
+
 Memory_Address_Register # (
-    .DATA_WIDTH(DATA_WIDTH)
-   ) mar (
+    .ADDR_WIDTH(ADDR_WIDTH)
+) mar (
     .clk(clk),
     .reset(reset),
     .load(load),
     .address_in(address_in),
-    .address_out(address_out));
+    .address_out(address_out)
+);
+
 initial begin
     clk = 0;
     forever #5 clk = ~clk; // Clock generation
 end
+
 initial begin
     // Test sequence
     $monitor("Time:%0t | clock=%b | reset=%b | load=%b | address_in=%h | address_out=%h", 
              $time, clk, reset, load, address_in, address_out);
-    reset = 1; load = 0; address_in = 32'h0000ABCD;
+    reset = 1; load = 0; address_in = 8'hAB;
     #10;
     
     reset = 0; 
@@ -34,7 +38,7 @@ initial begin
     #20;
 
     // Load another value
-    load = 1; address_in = 32'h12345678;
+    load = 1; address_in = 8'h78;
     #10;
     
     load = 0; 
@@ -48,5 +52,5 @@ initial begin
     #20;
 
     $stop;
-    end
+end
 endmodule

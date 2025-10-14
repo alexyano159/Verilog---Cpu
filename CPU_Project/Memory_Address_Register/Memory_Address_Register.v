d1e41cfd99122043b60fcb62991cf7ad4c55340e
Alex Yanovsky <alexyano159@gmail.com>
2025-10-14 14:48:00 +0300

    Refactor Memory_Address_Register to use ADDR_WIDTH

--- CPU_Project/Memory_Address_Register/Memory_Address_Register.v
+++ CPU_Project/Memory_Address_Register/Memory_Address_Register.v
@@ -1,17 +1,16 @@
 module Memory_Address_Register #(
-    parameter DATA_WIDTH = 32
+    parameter ADDR_WIDTH = 8
 )(
     input clk,
     input reset,
     input load,
-    //32 bit address bus,
-    input [DATA_WIDTH-1:0] address_in,
-    output reg [DATA_WIDTH-1:0] address_out
+    input [ADDR_WIDTH-1:0] address_in,
+    output reg [ADDR_WIDTH-1:0] address_out
 );
     always @(posedge clk or posedge reset) begin
-        if (reset) 
-            address_out <= {DATA_WIDTH{1'b0}}; // Clear address on reset
-        else if (load) 
-            address_out <= address_in; // Load new address
+        if (reset)
+            address_out <= {ADDR_WIDTH{1'b0}};
+        else if (load)
+            address_out <= address_in;
     end
 endmodule
